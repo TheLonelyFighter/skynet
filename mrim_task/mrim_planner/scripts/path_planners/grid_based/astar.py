30490b6540c217822bead44327aac9215a48bbf5
tianyilim <0.tianyi.lim@gmail.com>
2022-08-03 15:50:06 +0200

    Implementation and usage of A* instead of RRT(*)

--- mrim_task/mrim_planner/scripts/path_planners/grid_based/astar.py
+++ mrim_task/mrim_planner/scripts/path_planners/grid_based/astar.py
@@ -74,8 +74,6 @@ class Node:
 
         # Implement heuristic function as euclidean distance
         return np.linalg.norm([a,b,c])
-
-        raise NotImplementedError('[STUDENTS TODO] Heuristic function guiding the state space exploration not implemented. You have to finish it on your own.')
 # # #}
 
 # # #{ class AStar
@@ -102,16 +100,15 @@ class AStar():
         if len(path) <= 2:
             return path
 
-        raise NotImplementedError('[STUDENTS TODO] A*: path straightening is not finished. Finish it on your own.')
         # Tips:
         #  - divide the given path by a certain ratio and use this method recursively
 
-        # [STUDENTS TODO] REMOVE
         if self.grid.obstacleBetween(pt1, pt2):
 
-            # [STUDENTS TODO] Replace seg1 and seg2 variables effectively
-            seg1 = path[:1]
-            seg2 = path[1:]
+            # Split the path into rough halves.
+            # Recursively call the algorithm on the two halves.
+            seg1 = self.halveAndTest( path[:len(path)//2] )
+            seg2 = self.halveAndTest( path[len(path)//2:] )
 
             seg1.extend(seg2)
             return seg1
